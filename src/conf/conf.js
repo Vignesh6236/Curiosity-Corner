471491ffa1ec0032530ff39c45b3990cda98d209
Vignesh N <vigneshrage@gmail.com>
2024-11-08 13:17:37 +0530

    initial setup, setting up Appwrite and setting environment variables

---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+    appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+
+}
+
+
+
+export default conf
